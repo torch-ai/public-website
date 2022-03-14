17d236b0fee6c5746aabf11d7d34c984afa8b788
Kaleb Berry <kaleb.berry@outlook.com>
2022-03-14 02:27:14 -0500

    stuff policy nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67350f0d95258d68"; }
+/******/ 		__webpack_require__.h = function() { return "10ea4bc04656536d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
