0c70a3552fbb472e4cf32c0a743eb488c2f71cd8
Kaleb Berry <kaleb.berry@outlook.com>
2022-03-15 18:58:15 -0500

    this demo is kinda done?

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/global.scss';
+import { useEffect, useState } from 'react';
 import Layout from '../components/layout';
-import ReactFullpage from '@fullpage/react-fullpage';
+import Loader from '../components/loader';
 
 const MyApp = ({ Component, pageProps }) => {
+	// TODO add preloader to website.
+
+	useEffect(() => {
+		window.addEventListener('load', (event) => {
+			console.log('Content is loaded!');
+		});
+	});
+
 	return (
 		<Layout>
 			<Component {...pageProps} />
