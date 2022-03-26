3ba6a13e5cf6fe6d6431d4bf5e5156b72fe4efe5
Kaleb Berry <kaleb.berry@outlook.com>
2022-03-25 21:31:54 -0500

    on partner page

--- pages/company.js
+++ pages/company.js
@@ -74,12 +74,7 @@ const Index = ({ setNavColor, news }) => {
 				licenseKey={'A33F98B7-1BF24B82-AB8933EF-A1EC533E'}
 				navigation
 				verticalCentered={false}
-				responsiveSlides={true}
-				responsiveWidth={400}
-				scrollOverflowReset={true}
-				parallax={true}
-				parallaxKey={'EF2EC031-21464D53-B55BDB11-5FA39137'}
-				parallaxOptions={{ type: 'cover', percentage: 62, property: 'background' }}
+				responsiveWidth={600}
 				render={(props) => {
 					return (
 						<ReactFullpage.Wrapper>
