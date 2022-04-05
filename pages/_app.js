aed767bbb11d5fcaf10c50be577be74f1dc31fac
Kaleb Berry <kaleb.berry@outlook.com>
2022-04-05 11:53:07 -0500

    testing script

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,56 @@
-import '../styles/global.scss';
-import { useEffect, useState } from 'react';
-import Layout from '../components/layout';
-import Loader from '../components/loader';
+import "../styles/global.scss";
+import { useEffect, useState } from "react";
+import Layout from "../components/layout";
+import Head from "next/head";
+import Loader from "../components/loader";
 
 const MyApp = ({ Component, pageProps }) => {
-	// TODO add preloader to website.
+  // TODO add preloader to website.
 
-	useEffect(() => {
-		window.addEventListener('load', (event) => {
-			console.log('Content is loaded!');
-		});
-	});
+  useEffect(() => {
+    (window.heap = window.heap || []),
+      (heap.load = function (e, t) {
+        (window.heap.appid = e), (window.heap.config = t = t || {});
+        var r = document.createElement("script");
+        (r.type = "text/javascript"),
+          (r.async = !0),
+          (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
+        var a = document.getElementsByTagName("script")[0];
+        a.parentNode.insertBefore(r, a);
+        for (
+          var n = function (e) {
+              return function () {
+                heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+              };
+            },
+            p = [
+              "addEventProperties",
+              "addUserProperties",
+              "clearEventProperties",
+              "identify",
+              "resetIdentity",
+              "removeEventProperty",
+              "setEventProperties",
+              "track",
+              "unsetEventProperty",
+            ],
+            o = 0;
+          o < p.length;
+          o++
+        )
+          heap[p[o]] = n(p[o]);
+      });
+    heap.load("2334335890");
+  });
 
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <>
+      <Head></Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 };
 
 export default MyApp;
