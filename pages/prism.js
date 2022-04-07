7c10e3bd254d1cafb6535df9b0bfeb980b3efe1c
Kaleb Berry <kaleb.berry@outlook.com>
2022-04-07 14:42:39 -0500

    adding to prism test

--- pages/prism.js
+++ pages/prism.js
@@ -1,128 +1,170 @@
 import { InView } from "react-intersection-observer";
 import ReactPlayer from "react-player";
+import ReactFullpage from "@fullpage/react-fullpage";
 import Footer from "../components/footer";
+import Head from "next/head";
 
 const Prism = ({ setNavColor }) => {
   return (
     <>
-      <section className={`prismHero`}>
-        <InView
-          as="div"
-          onChange={(inView, entry) => setNavColor(inView ? "black" : "white")}
-        >
-          <div className={`container`}>
-            <h1>TRUST</h1>
-            <h4>
-              As an insurance professional, you enable trust across entire
-              economies.
-            </h4>
-          </div>
-        </InView>
-      </section>
+      <Head>
+        <title>
+          World's Most Trusted AI Platform | Unlock Human Potential | Torch.AI
+        </title>
+      </Head>
+      <ReactFullpage
+        navigation
+        verticalCentered={false}
+        responsiveWidth={1500}
+        render={(props) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <section className={`section hero`}>
+                <div className="container">
+                  <h1>PRISM</h1>
+                  <h4>
+                    Military-grade document intelligence. Now for insurance.
+                  </h4>
+                </div>
+              </section>
+              <section className={`section professional`}>
+                <InView
+                  as="div"
+                  className="checking"
+                  onChange={(inView, entry) =>
+                    setNavColor(inView ? "black" : "white")
+                  }
+                >
+                  <div className="container">
+                    <h3>
+                      As an insurance professional, you enable trust across
+                      entire economies.
+                    </h3>
+                    <p>
+                      Prism powers that mission, simplifying how you achieve
+                      instant value from critical document data-faster, cheaper,
+                      more accurate than anyone. else.
+                    </p>
+                  </div>
+                </InView>
+              </section>
+              <section className={`section organization`}>
+                <div className="container">
+                  <h3>Insurance organizations deal with messy data.</h3>
+                  <p className="subtitle">
+                    Most of it—up to 90%—consists of documents, PDFs, scans,
+                    faxes, and images.
+                  </p>
+                  <hr />
+                  <div className="prismFeatures">
+                    <div className="prismFeature-item">
+                      <h5>It's Expensive.</h5>
+                      <p>
+                        It can cost up to $220 to analyze a single document.
+                      </p>
+                    </div>
+                    <div className="prismFeature-item">
+                      <h5>It's not productive.</h5>
+                      <p>
+                        >60% of teams are manually copying and pasting document
+                        data
+                      </p>
+                    </div>
+                    <div className="prismFeature-item">
+                      <h5>It's not accurate.</h5>
+                      <p>
+                        Manual efforts are error prone and existing solutions
+                        can't scale.
+                      </p>
+                    </div>
+                    <div className="prismFeature-item">
+                      <h5>It's getting worse, fast.</h5>
+                      <p>
+                        Messy data is growing 300% faster than more usable,
+                        tabular data.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
 
-      <section className="prismAboutTorch">
-        <div className="container">
-          <h5>
-            Torch.AI is the world leader in document intelligence. With the
-            launch of <strong className="prismAccent">Prism</strong>, we now
-            bring our military-grade AI technology to the insurance industry to
-            support your mission to create trust.
-          </h5>
-        </div>
-      </section>
-
-      <section className="prismIntro">
-        <div className="container">
-          <h2>
-            Introducing <span className="prismAccent">Prism</span>, <br /> a
-            product of Nexus
-            <sup>™</sup>
-          </h2>
-          <h5>Make your data totally available, usable, valuable.</h5>
-          <div className="prismFeatures">
-            <ul className="prismFeatures-list">
-              <li>
-                <h5>
-                  Document processing for insurance policies, claims, contracts,
-                  COIs, and other relevant documents
-                </h5>
-              </li>
-              <li>
-                <h5>
-                  Risk handling and operational process automation for improved
-                  employee productivity.
-                </h5>
-              </li>
-              <li>
-                <h5>
-                  Over 90% average cost savings, less than 9.8 millisecond
-                  processing speed per page, and 96% accuracy of data
-                  extraction.
-                </h5>
-              </li>
-            </ul>
-          </div>
-          <div className="prismVideo">
-            <ReactPlayer
-              controls="true"
-              url="https://vimeo.com/695913201"
-              className="react-player"
-            />
-
-            {/* <button> Download PDF</button> */}
-          </div>
-        </div>
-      </section>
-
-      <section className="prismLearnMore">
-        <div className="container">
-          <h3>
-            To learn more about <span className="prismAccent">Prism</span>,
-            contact us below.
-          </h3>
-
-          <form action="https://formspree.io/f/mvolablv" method="POST">
-            <div>
-              <label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Full Name"
-                  required
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="text"
-                  name="company"
-                  placeholder="Company"
-                  required
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <input type="email" name="email" placeholder="Email" required />
-              </label>
-            </div>
-            <div>
-              <label>
-                <textarea
-                  name="message"
-                  placeholder="Message"
-                  required
-                ></textarea>
-              </label>
-            </div>
-            <div>
-              <button type="submit">Send</button>
-            </div>
-          </form>
-        </div>
-      </section>
-      <Footer />
+              <section className={`section win`}>
+                <div className="container">
+                  <div className="winPoints">
+                    <h3>Win with Prism</h3>
+                    <ul>
+                      <li>Reduce operational cost.</li>
+                      <li>Automate & accelerate document processing times.</li>
+                      <li>
+                        Achieve a seamless experience for your teams. Enable
+                        trust for your clients.
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="prismVideo">
+                    <video controls poster="./thumbnail.jpg">
+                      <source src="./videos/prismVideo.mp4" type="video/mp4" />
+                    </video>
+                  </div>
+                </div>
+              </section>
+              <section className={`section contact`}>
+                <div className="container">
+                  <h4>
+                    Learn why Torch.AI is the world leader in document
+                    intelligence.
+                  </h4>
+                  <form action="https://formspree.io/f/mvolablv" method="POST">
+                    <div>
+                      <label>
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Full Name"
+                          required
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        <input
+                          type="text"
+                          name="company"
+                          placeholder="Company"
+                          required
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        <input
+                          type="email"
+                          name="email"
+                          placeholder="Email"
+                          required
+                        />
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        <textarea
+                          name="message"
+                          placeholder="Message"
+                          required
+                        ></textarea>
+                      </label>
+                    </div>
+                    <div>
+                      <button type="submit">Send</button>
+                    </div>
+                  </form>
+                </div>
+              </section>
+              <Footer />
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </>
   );
 };
