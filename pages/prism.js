63eb67543cbc96a960e22839aa8d7e0d5d5c0953
Kaleb Berry <kaleb.berry@outlook.com>
2022-04-07 07:24:05 -0500

    adding logic

--- pages/prism.js
+++ pages/prism.js
@@ -1,130 +1,34 @@
-import { InView } from "react-intersection-observer";
-import ReactPlayer from "react-player";
-import Footer from "../components/footer";
+import { InView } from 'react-intersection-observer';
+import ReactPlayer from 'react-player';
+import ReactFullpage from '@fullpage/react-fullpage';
+import Footer from '../components/footer';
+import Head from 'next/head';
 
 const Prism = ({ setNavColor }) => {
-  return (
-    <>
-      <section className={`prismHero`}>
-        <InView
-          as="div"
-          onChange={(inView, entry) => setNavColor(inView ? "black" : "white")}
-        >
-          <div className={`container`}>
-            <h1>TRUST</h1>
-            <h4>
-              As an insurance professional, you enable trust across entire
-              economies.
-            </h4>
-          </div>
-        </InView>
-      </section>
-
-      <section className="prismAboutTorch">
-        <div className="container">
-          <h5>
-            Torch.AI is the world leader in document intelligence. With the
-            launch of <strong className="prismAccent">Prism</strong>, we now
-            bring our military-grade AI technology to the insurance industry to
-            support your mission to create trust.
-          </h5>
-        </div>
-      </section>
-
-      <section className="prismIntro">
-        <div className="container">
-          <h2>
-            Introducing <span className="prismAccent">Prism</span>, <br /> a
-            product of Nexus
-            <sup>™</sup>
-          </h2>
-          <h5>Make your data totally available, usable, valuable.</h5>
-          <div className="prismFeatures">
-            <ul className="prismFeatures-list">
-              <li>
-                <h5>
-                  Document processing for insurance policies, claims, contracts,
-                  COIs, and other relevant documents
-                </h5>
-              </li>
-              <li>
-                <h5>
-                  Risk handling and operational process automation for improved
-                  employee productivity.
-                </h5>
-              </li>
-              <li>
-                <h5>
-                  Over 90% average cost savings, less than 9.8 millisecond
-                  processing speed per page, and 96% accuracy of data
-                  extraction.
-                </h5>
-              </li>
-            </ul>
-          </div>
-          <div className="prismVideo">
-            <ReactPlayer
-              controls="true"
-              url="https://vimeo.com/695913201"
-              className="react-player"
-            />
-
-            {/* <button> Download PDF</button> */}
-          </div>
-        </div>
-      </section>
-
-      <section className="prismLearnMore">
-        <div className="container">
-          <h3>
-            To learn more about <span className="prismAccent">Prism</span>,
-            contact us below.
-          </h3>
-
-          <form action="https://formspree.io/f/mvolablv" method="POST">
-            <div>
-              <label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Full Name"
-                  required
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="text"
-                  name="company"
-                  placeholder="Company"
-                  required
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <input type="email" name="email" placeholder="Email" required />
-              </label>
-            </div>
-            <div>
-              <label>
-                <textarea
-                  name="message"
-                  placeholder="Message"
-                  required
-                ></textarea>
-              </label>
-            </div>
-            <div>
-              <button type="submit">Send</button>
-            </div>
-          </form>
-        </div>
-      </section>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>World's Most Trusted AI Platform | Unlock Human Potential | Torch.AI</title>
+			</Head>
+			<ReactFullpage
+				navigation
+				verticalCentered={false}
+				responsiveWidth={1500}
+				render={(props) => {
+					return (
+						<ReactFullpage.Wrapper>
+							<section className={`section hero`}></section>
+							<section className={`section statement`}></section>
+							<section className={`section nexus`}></section>
+							<section className={`section data`}></section>
+							<section className={`section impact`}></section>
+							<Footer />
+						</ReactFullpage.Wrapper>
+					);
+				}}
+			/>
+		</>
+	);
 };
 
 export default Prism;
