a0cbc289099be4bd8c728d0d6c874029f8b34334
Kaleb Berry <kaleb.berry@outlook.com>
2022-04-13 11:02:23 -0500

    updated redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-	images: {
-		domains: ['images.ctfassets.net'],
-	},
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+  async redirects() {
+    return [
+      {
+        source:
+          "/post/dataedge-becomes-torch-ai-value-added-reseller-for-cyber-solutions",
+        destination:
+          "/post/dataedge-seelcts-torch-ai-for-ai-enabled-data-rediness-solutions",
+        permanent: true,
+      },
+    ];
+  },
 };
