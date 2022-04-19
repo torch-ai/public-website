f98a33d497add714fba154b090cf9a0557cc4dbd
Lance Gliser <lance.gliser@torch.ai>
2022-04-19 15:29:17 -0400

    'var' is used instead of 'let' or 'const'

--- pages/_app.js
+++ pages/_app.js
@@ -11,34 +11,30 @@ const MyApp = ({ Component, pageProps }) => {
     (window.heap = window.heap || []),
       (heap.load = function (e, t) {
         (window.heap.appid = e), (window.heap.config = t = t || {});
-        var r = document.createElement("script");
+        const r = document.createElement("script");
         (r.type = "text/javascript"),
           (r.async = !0),
           (r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js");
-        var a = document.getElementsByTagName("script")[0];
+        const a = document.getElementsByTagName("script")[0];
         a.parentNode.insertBefore(r, a);
-        for (
-          var n = function (e) {
-              return function () {
-                heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-              };
-            },
-            p = [
-              "addEventProperties",
-              "addUserProperties",
-              "clearEventProperties",
-              "identify",
-              "resetIdentity",
-              "removeEventProperty",
-              "setEventProperties",
-              "track",
-              "unsetEventProperty",
-            ],
-            o = 0;
-          o < p.length;
-          o++
-        )
-          heap[p[o]] = n(p[o]);
+        const n = function (e) {
+            return function () {
+              heap.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+            };
+          },
+          p = [
+            "addEventProperties",
+            "addUserProperties",
+            "clearEventProperties",
+            "identify",
+            "resetIdentity",
+            "removeEventProperty",
+            "setEventProperties",
+            "track",
+            "unsetEventProperty",
+          ];
+        let o = 0;
+        for (; o < p.length; o++) heap[p[o]] = n(p[o]);
       });
     heap.load("1628599215");
   });
