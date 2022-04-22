eb9f8dab5bc9e4d0aaa219b560f254299e8171cd
Lance Gliser <lance.gliser@torch.ai>
2022-04-22 13:46:59 -0400

    Deleted the Blog and Recipe types.

--- generated/contentful/index.ts
+++ generated/contentful/index.ts
@@ -1,6 +1,4 @@
-export type { TypeBlogPost, TypeBlogPostFields } from "./TypeBlogPost";
 export type { TypeCareerPosting, TypeCareerPostingFields } from "./TypeCareerPosting";
 export type { TypeNews, TypeNewsFields } from "./TypeNews";
 export type { TypePage, TypePageFields } from "./TypePage";
 export type { TypePageSection, TypePageSectionFields } from "./TypePageSection";
-export type { TypeRecipe, TypeRecipeFields } from "./TypeRecipe";
