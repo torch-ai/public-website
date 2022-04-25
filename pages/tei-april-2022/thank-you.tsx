a06898d027d45d725636cf93dae6e7cbdfe66e24
Lance Gliser <lance.gliser@torch.ai>
2022-04-25 12:27:10 -0400

    An initial approach using local design assets

---
+++ pages/tei-april-2022/thank-you.tsx
@@ -0,0 +1,49 @@
+// noinspection JSUnusedGlobalSymbols
+
+import React, { ReactElement } from "react";
+import Footer from "../../components/Footer";
+import Head from "next/head";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { TypePage } from "../../generated/contentful";
+import { getPage } from "../../contentful/client";
+import Styles from "./styles.module.scss";
+import ContentfulContent from "../../components/ContentfulContent/ContentfulContent";
+import Header from "./components/Header/Header";
+
+export const getStaticProps: GetStaticProps<{
+  page: TypePage;
+}> = async () => {
+  // See: https://app.contentful.com/spaces/dtb5w0ega2aw/entries/38wyUNS9a6o51vmi5Sdxiy
+  const page = await getPage("38wyUNS9a6o51vmi5Sdxiy");
+
+  return {
+    props: {
+      page,
+    },
+  };
+};
+
+const ThankYou = ({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>): ReactElement => {
+  const {
+    fields: { title, content },
+  } = page;
+
+  return (
+    <>
+      <Head>
+        <title>{title} | Torch.AI</title>
+      </Head>
+      <section>
+        <Header page={page} />
+        <main className={Styles.main}>
+          <ContentfulContent content={content} />
+        </main>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default ThankYou;
