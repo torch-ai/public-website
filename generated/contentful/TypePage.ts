a82b666de8948f4dd65e556350aa706e454ded58
Lance Gliser <lance.gliser@torch.ai>
2022-04-25 15:27:38 -0400

    Regenerated the TypePage entry with the header fields.

--- generated/contentful/TypePage.ts
+++ generated/contentful/TypePage.ts
@@ -5,6 +5,11 @@ export interface TypePageFields {
     title: Contentful.EntryFields.Symbol;
     subtitle?: Contentful.EntryFields.Symbol;
     hasSections?: Contentful.EntryFields.Boolean;
+    integrationDetails: Contentful.EntryFields.Symbol;
+    headerBackgroundColor?: Contentful.EntryFields.Symbol;
+    headerBackgroundImage?: Contentful.Asset;
+    headerForegroundColor?: Contentful.EntryFields.Symbol;
+    headerImage?: Contentful.Asset;
     content?: CFRichTextTypes.Block | CFRichTextTypes.Inline;
 }
 
