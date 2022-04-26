655253d448d477748b11036ddb2eca944f04024f
Lance Gliser <lance.gliser@torch.ai>
2022-04-26 18:27:40 -0400

    A bit of console logging while building and proper image alt description.

--- components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
+++ components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
@@ -8,15 +8,21 @@ import Styles from "./styles.module.scss";
 interface Props {
   asset: Asset;
 }
-const ContentfulImage: React.FunctionComponent<Props> = ({ asset }) => (
-  <div className={Styles.wrapper}>
-    <Image
-      src={["https://", asset.fields.file.url].join("")}
-      width={asset.fields.file.details.image.width}
-      height={asset.fields.file.details.image.height}
-      alt={"Title page of the Forrester economic impact report"}
-    />
-  </div>
-);
+const ContentfulImage: React.FunctionComponent<Props> = ({ asset }) => {
+  console.info(
+    "ContentfulImage:",
+    ["https://", asset.fields.file.url].join("")
+  );
+  return (
+    <div className={Styles.wrapper}>
+      <Image
+        src={["https://", asset.fields.file.url].join("")}
+        width={asset.fields.file.details.image.width}
+        height={asset.fields.file.details.image.height}
+        alt={asset.fields.description || undefined}
+      />
+    </div>
+  );
+};
 
 export default ContentfulImage;
