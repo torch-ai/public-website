0ca700792f785aff016244aeca2c19e184e449b6
Lance Gliser <lance.gliser@torch.ai>
2022-04-26 18:30:15 -0400

    Whoops. Included protocol when it's a relative protocol already.

--- components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
+++ components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
@@ -8,21 +8,15 @@ import Styles from "./styles.module.scss";
 interface Props {
   asset: Asset;
 }
-const ContentfulImage: React.FunctionComponent<Props> = ({ asset }) => {
-  console.info(
-    "ContentfulImage:",
-    ["https://", asset.fields.file.url].join("")
-  );
-  return (
-    <div className={Styles.wrapper}>
-      <Image
-        src={["https://", asset.fields.file.url].join("")}
-        width={asset.fields.file.details.image.width}
-        height={asset.fields.file.details.image.height}
-        alt={asset.fields.description || undefined}
-      />
-    </div>
-  );
-};
+const ContentfulImage: React.FunctionComponent<Props> = ({ asset }) => (
+  <div className={Styles.wrapper}>
+    <Image
+      src={["https:", asset.fields.file.url].join("")}
+      width={asset.fields.file.details.image.width}
+      height={asset.fields.file.details.image.height}
+      alt={asset.fields.description || undefined}
+    />
+  </div>
+);
 
 export default ContentfulImage;
