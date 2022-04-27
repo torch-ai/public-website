c973f549320c77a7198c52ddf2fed26aa550247a
Lance Gliser <lance.gliser@torch.ai>
2022-04-27 12:20:19 -0400

    Created <Button />

---
+++ components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import clsx from "clsx";
+import Styles from "./styles.module.scss";
+
+interface Props {
+  isPrimary?: boolean;
+  isSecondary?: boolean;
+}
+const Button: React.FunctionComponent<
+  Props & React.HTMLAttributes<HTMLButtonElement>
+> = ({ isPrimary, isSecondary, children, ...props }) => (
+  <button
+    {...props}
+    className={clsx(Styles.default, props.className, {
+      [Styles.isPrimary]: isPrimary,
+      [Styles.isSecondary]: isSecondary,
+    })}
+  >
+    {children}
+  </button>
+);
+export default Button;
