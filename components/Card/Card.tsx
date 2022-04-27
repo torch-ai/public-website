8724956ba9cf731856de1ad3aee24bb903b63241
Lance Gliser <lance.gliser@torch.ai>
2022-04-27 08:43:39 -0400

    Established a border color variable and used it in the Card border and TEI form elements

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -4,21 +4,19 @@ import Styles from "./styles.module.scss";
 
 interface Props {
   isSquare?: boolean;
+  hasBorder?: boolean;
   elevation?: number;
 }
 const Card: React.FunctionComponent<
   Props & React.HTMLAttributes<HTMLDivElement>
-> = ({ isSquare, elevation = 2, children, ...props }) => (
+> = ({ isSquare, hasBorder = true, elevation = 2, children, ...props }) => (
   <article
     {...props}
-    className={clsx(
-      Styles.card,
-      props.className,
-      Styles[`elevation${elevation}`],
-      {
-        [Styles.square]: isSquare,
-      }
-    )}
+    className={clsx(Styles.card, props.className, {
+      [Styles.square]: isSquare,
+      [Styles.border]: hasBorder,
+      [Styles[`elevation${elevation}`]]: elevation,
+    })}
   >
     {children}
   </article>
