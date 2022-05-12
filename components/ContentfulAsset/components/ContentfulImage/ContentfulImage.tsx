8615dc0b2c953ba53a34ce1126529a7271c25118
Lance Gliser <lance.gliser@torch.ai>
2022-05-12 16:27:24 -0400

    Added <ContentfulVideo /> to support <ContentfulAsset />

--- components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
+++ components/ContentfulAsset/components/ContentfulImage/ContentfulImage.tsx
@@ -3,20 +3,17 @@
 import React from "react";
 import Image from "next/image";
 import { Asset } from "contentful";
-import Styles from "./styles.module.scss";
 
 interface Props {
   asset: Asset;
 }
 const ContentfulImage: React.FunctionComponent<Props> = ({ asset }) => (
-  <div className={Styles.wrapper}>
-    <Image
-      src={["https:", asset.fields.file.url].join("")}
-      width={asset.fields.file.details.image.width}
-      height={asset.fields.file.details.image.height}
-      alt={asset.fields.description || undefined}
-    />
-  </div>
+  <Image
+    src={["https:", asset.fields.file.url].join("")}
+    width={asset.fields.file.details.image.width}
+    height={asset.fields.file.details.image.height}
+    alt={asset.fields.description || undefined}
+  />
 );
 
 export default ContentfulImage;
