49f9e5d60f3b3d6fa9e3abcfbc3b97894eabdac1
Lance Gliser <lance.gliser@torch.ai>
2022-05-12 16:27:24 -0400

    Added <ContentfulVideo /> to support <ContentfulAsset />

---
+++ components/ContentfulAsset/components/ContentfulVideo/ContentfulVideo.tsx
@@ -0,0 +1,16 @@
+// noinspection JSUnusedGlobalSymbols
+
+import React from "react";
+import { Asset } from "contentful";
+import Styles from "./styles.module.scss";
+
+interface Props {
+  asset: Asset;
+}
+const ContentfulVideo: React.FunctionComponent<Props> = ({ asset }) => (
+  <video title={asset.fields.title} className={Styles.video} controls>
+    <source src={asset.fields.file.url} type={asset.fields.file.contentType} />
+  </video>
+);
+
+export default ContentfulVideo;
