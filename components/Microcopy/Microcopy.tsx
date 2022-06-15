9689078f0f2bf9d424fca772ed6d0364d1ce528b
Grayson Martin <grayson.martin@torch.ai>
2022-06-15 10:20:05 -0500

    Add editing mode

--- components/Microcopy/Microcopy.tsx
+++ components/Microcopy/Microcopy.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import { TypeMicrocopy } from "../../generated/contentful";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import Style from "./styles.module.scss";
 
 export interface MicrocopyProps {
   entries: TypeMicrocopy[];
@@ -11,10 +12,48 @@ export interface MicrocopyProps {
 const Microcopy: FunctionComponent<MicrocopyProps> = ({ entries, id }) => {
   let microcopy = entries.find((entry) => entry.sys.id == id);
 
-  if (microcopy) {
-    return <>{microcopy.fields.content}</>;
+  if (
+    typeof window !== "undefined" &&
+    window.sessionStorage.getItem("editingMode")
+  ) {
+    // client side
+    if (microcopy) {
+      if (microcopy) {
+        return (
+          <span
+            className={Style.editing}
+            onClick={() => {
+              window.open(
+                `https://app.contentful.com/spaces/dtb5w0ega2aw/entries/${id}`,
+                "_blank"
+              );
+            }}
+          >
+            {microcopy.fields.content}
+          </span>
+        );
+      } else {
+        return (
+          <span
+            className={Style.editing}
+            onClick={() => {
+              window.open(
+                `https://app.contentful.com/spaces/dtb5w0ega2aw/entries/${id}`,
+                "_blank"
+              );
+            }}
+          >
+            <FontAwesomeIcon icon={faTriangleExclamation} />
+          </span>
+        );
+      }
+    }
   } else {
-    return <FontAwesomeIcon icon={faTriangleExclamation} />;
+    if (microcopy) {
+      return <>{microcopy.fields.content}</>;
+    } else {
+      return <FontAwesomeIcon icon={faTriangleExclamation} />;
+    }
   }
 };
 
