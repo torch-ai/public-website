288ac748bb824a26fd6dbac02467fdb4b87b3f31
Grayson Martin <grayson.martin@torch.ai>
2022-06-15 13:11:37 -0500

    Clean up if statements

--- components/Microcopy/Microcopy.tsx
+++ components/Microcopy/Microcopy.tsx
@@ -17,43 +17,33 @@ const Microcopy: FunctionComponent<MicrocopyProps> = ({ entries, id }) => {
     window.sessionStorage.getItem("editingMode")
   ) {
     // client side
-    if (microcopy) {
-      if (microcopy) {
-        return (
-          <span
-            className={Style.editing}
-            onClick={() => {
-              window.open(
-                `https://app.contentful.com/spaces/dtb5w0ega2aw/entries/${id}`,
-                "_blank"
-              );
-            }}
-          >
-            {microcopy.fields.content}
-          </span>
-        );
-      } else {
-        return (
-          <span
-            className={Style.editing}
-            onClick={() => {
-              window.open(
-                `https://app.contentful.com/spaces/dtb5w0ega2aw/entries/${id}`,
-                "_blank"
-              );
-            }}
-          >
-            <FontAwesomeIcon icon={faTriangleExclamation} />
-          </span>
-        );
-      }
-    }
+    return (
+      <span
+        className={Style.editing}
+        onClick={() => {
+          window.open(
+            `https://app.contentful.com/spaces/dtb5w0ega2aw/entries/${id}`,
+            "_blank"
+          );
+        }}
+      >
+        {microcopy ? (
+          microcopy.fields.content
+        ) : (
+          <FontAwesomeIcon icon={faTriangleExclamation} />
+        )}
+      </span>
+    );
   } else {
-    if (microcopy) {
-      return <>{microcopy.fields.content}</>;
-    } else {
-      return <FontAwesomeIcon icon={faTriangleExclamation} />;
-    }
+    return (
+      <>
+        {microcopy ? (
+          microcopy.fields.content
+        ) : (
+          <FontAwesomeIcon icon={faTriangleExclamation} />
+        )}
+      </>
+    );
   }
 };
 
